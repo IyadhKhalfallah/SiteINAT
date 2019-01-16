2350951dd21aaf44c517afb9ca0b6f30dbb5dc2f
HAMDI <benyaflahhamdi2@gmail.com>
2019-01-16 10:27:49 +0100

    navbar + footer + events

--- projet/src/app/app.module.ts
+++ projet/src/app/app.module.ts
@@ -5,16 +5,55 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { EventsComponent } from './components/events/events.component';
+import { FormationsComponent } from './components/formations/formations.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SponsorsPartenaireComponent } from './components/sponsors-partenaire/sponsors-partenaire.component';
+import { StructureComponent } from './components/structure/structure.component';
+import { RseComponent } from './components/rse/rse.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DetailsEventComponent } from './components/events/details-event/details-event.component';
+
+const routes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'home' , component: HomeComponent},
+  {path: 'projects' , component: ProjectsComponent},
+  {path: 'events' , component: EventsComponent},
+  {path: 'formations' , component: FormationsComponent},
+  {path: 'contact' , component: ContactComponent},
+  {path: 'aboutUs' , component: AboutUsComponent},
+  {path: 'sponsors' , component: SponsorsPartenaireComponent},
+  {path: 'structure' , component: StructureComponent},
+  {path: 'rse' , component: RseComponent},
+
+  {path: '**' , component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    EventsComponent,
+    FormationsComponent,
+    ContactComponent,
+    AboutUsComponent,
+    SponsorsPartenaireComponent,
+    StructureComponent,
+    RseComponent,
+    NotFoundComponent,
+    DetailsEventComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projet/src/app/components/sponsors-partenaire/sponsors-partenaire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsors-partenaire',
+  templateUrl: './sponsors-partenaire.component.html',
+  styleUrls: ['./sponsors-partenaire.component.css']
+})
+export class SponsorsPartenaireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
