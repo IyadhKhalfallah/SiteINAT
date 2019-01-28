704c33335f1f8f675a9353821ad951cd957b388c
Unknown <gharsallihelmi@gmail.com>
2019-01-28 03:20:10 +0100

    strcuture with data

--- projet/Front-end/src/app/components/sponsors-partenaire/sponsors-partenaire.component.ts
+++ projet/Front-end/src/app/components/sponsors-partenaire/sponsors-partenaire.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponsors-partenaire.component.css']
 })
 export class SponsorsPartenaireComponent implements OnInit {
+  sponsors = [
+      {id: 1, name: 'sponsor1' , image: '5.png'},
+      {id: 2, name: 'sponsor2' , image: '6.png'},
+      {id: 3, name: 'sponsor3' , image: '7.png'},
+      {id: 4, name: 'sponsor4' , image: '5.png'},
+      {id: 5, name: 'sponsor5' , image: '6.png'}
+      ];
+  partners = [
+      {id: 1, name: 'partner1', image: '5.png'},
+      {id: 1, name: 'partner2', image: '6.png'}
+      ];
 
   constructor() { }
 
