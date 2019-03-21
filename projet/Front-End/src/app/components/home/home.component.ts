d0511b2fd352d4d9a46a063611528c00597b6e49
Unknown <gharsallihelmi@gmail.com>
2019-03-21 20:49:53 +0100

    update sponsors & media + fix membres

--- projet/Front-End/src/app/components/home/home.component.ts
+++ projet/Front-End/src/app/components/home/home.component.ts
@@ -50,7 +50,38 @@ export class HomeComponent implements OnInit {
     }
   ];
 
-  sponsors = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
+    sponsors = [
+        { name: 'Président' , image: '_Président_(fromage).png'},
+        { name: 'Agil' , image: 'Agil.png'},
+        { name: 'Alfa' , image: 'Alfa.png'},
+        { name: 'APIA' , image: 'APIA.png'},
+        { name: 'B lounge' , image: 'B lounge.jpg'},
+        {name: 'Bayer' , image: 'Bayer.png'},
+        { name: 'Ben Yedder' , image: 'Ben Yedder.png'},
+        { name: 'BFI' , image: 'BFI.png'},
+        { name: 'BIAT' , image: 'BIAT.png'},
+        { name: 'BNA' , image: 'BNA.png'},
+        { name: 'Cogite' , image: 'Cogite.png'},
+        { name: 'Comete Engineering' , image: 'Comete Engineering.png'},
+        { name: 'ILMS' , image: 'ILMS.png'},
+        { name: 'IMS' , image: 'IMS.png'},
+        { name: 'SCET' , image: 'logo-SCET.svg'},
+        { name: 'Mabrouka' , image: 'Mabrouka.png'},
+        { name: 'OHN' , image: 'ONH.png'},
+        { name: 'Orange Tunisie' , image: 'Orange-Tunisie_logo.png'},
+        { name: 'OVITA' , image: 'OVITA.png'},
+        { name: 'RED bull' , image: 'RED bull.png'},
+        { name: 'Ruspina' , image: 'Ruspina.png'},
+        { name: 'Sadira' , image: 'Sadira.png'},
+        { name: 'SFBT' , image: 'SFBT.png'},
+        { name: 'SNA' , image: 'SNA.png'},
+        { name: 'STB' , image: 'STB.png'},
+        { name: 'Toast' , image: 'Toast.jpg'},
+        { name: 'Toyota' , image: 'Toyota.png'},
+        { name: 'Tunisie Telecom' , image: 'tunisie-teleco.png'},
+        { name: 'UTAP' , image: 'UTAP.png'},
+        { name: 'Vitalait' , image: 'vitalait.png'}
+    ];
 
   constructor(
     private projectService: ProjectsService,
--- projet/Front-End/src/app/components/sponsors-partenaire/sponsors-partenaire.component.ts
+++ projet/Front-End/src/app/components/sponsors-partenaire/sponsors-partenaire.component.ts
@@ -7,16 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorsPartenaireComponent implements OnInit {
   sponsors = [
-      {id: 1, name: 'sponsor1' , image: '5.png'},
-      {id: 2, name: 'sponsor2' , image: '6.png'},
-      {id: 3, name: 'sponsor3' , image: '7.png'},
-      {id: 4, name: 'sponsor4' , image: '5.png'},
-      {id: 5, name: 'sponsor5' , image: '6.png'}
+      { name: 'Président' , image: '_Président_(fromage).png'},
+      { name: 'Agil' , image: 'Agil.png'},
+      { name: 'Alfa' , image: 'Alfa.png'},
+      { name: 'APIA' , image: 'APIA.png'},
+      { name: 'B lounge' , image: 'B lounge.jpg'},
+      {name: 'Bayer' , image: 'Bayer.png'},
+      { name: 'Ben Yedder' , image: 'Ben Yedder.png'},
+      { name: 'BFI' , image: 'BFI.png'},
+      { name: 'BIAT' , image: 'BIAT.png'},
+      { name: 'BNA' , image: 'BNA.png'},
+      { name: 'Cogite' , image: 'Cogite.png'},
+      { name: 'Comete Engineering' , image: 'Comete Engineering.png'},
+      { name: 'ILMS' , image: 'ILMS.png'},
+      { name: 'IMS' , image: 'IMS.png'},
+      { name: 'SCET' , image: 'logo-SCET.svg'},
+      { name: 'Mabrouka' , image: 'Mabrouka.png'},
+      { name: 'OHN' , image: 'ONH.png'},
+      { name: 'Orange Tunisie' , image: 'Orange-Tunisie_logo.png'},
+      { name: 'OVITA' , image: 'OVITA.png'},
+      { name: 'RED bull' , image: 'RED bull.png'},
+      { name: 'Ruspina' , image: 'Ruspina.png'},
+      { name: 'Sadira' , image: 'Sadira.png'},
+      { name: 'SFBT' , image: 'SFBT.png'},
+      { name: 'SNA' , image: 'SNA.png'},
+      { name: 'STB' , image: 'STB.png'},
+      { name: 'Toast' , image: 'Toast.jpg'},
+      { name: 'Toyota' , image: 'Toyota.png'},
+      { name: 'Tunisie Telecom' , image: 'tunisie-teleco.png'},
+      { name: 'UTAP' , image: 'UTAP.png'},
+      { name: 'Vitalait' , image: 'vitalait.png'}
       ];
   partners = [
-      {id: 1, name: 'partner1', image: '5.png'},
-      {id: 1, name: 'partner2', image: '6.png'}
-      ];
+      { name: 'CAP FM', image: 'CAP FM.png'},
+      { name: 'Express FM', image: 'Express_FM.png'},
+      { name: 'IFM', image: 'IFM.png'},
+      { name: 'Jawhara FM', image: 'Jawhara Fm.png'},
+      { name: 'Mosaique FM', image: 'Mosaique Fm.png'},
+      { name: 'Radio Nationale', image: 'Radio Nationale.png'},
+      { name: 'RTCI', image: 'RTCI.png'},
+      { name: 'Saraha FM', image: 'Saraha FM.png'},
+
+  ];
 
   constructor() { }
 
