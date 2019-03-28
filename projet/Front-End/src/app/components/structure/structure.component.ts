5f4a9f59b39ed7f2c21741fc397b47f8b85e5eb8
Unknown <gharsallihelmi@gmail.com>
2019-03-28 18:34:00 +0100

    reseaux sociaux + fondateur

--- projet/Front-End/src/app/components/structure/structure.component.ts
+++ projet/Front-End/src/app/components/structure/structure.component.ts
@@ -12,14 +12,18 @@ export class StructureComponent implements OnInit {
             image: 'president.JPG',
             quote: '"  Plus le combat est grand plus la victoire est immense "' ,
             tel: '+216 25 865 393' ,
-            email: 'aoursouene@gmail.com' },
+            email: 'aoursouene@gmail.com' ,
+            fb: 'https://www.facebook.com/oursouene.ayari' ,
+            lin: 'https://www.linkedin.com/in/mohamed-oursouene-ayari-53b879154/' },
 
         {   name : 'Marriem Aouadi' ,
             post : 'VP' ,
             image: 'VP.jpg',
             quote: '" Se réunir est un début, rester ensemble est un progrès, travailler ensemble est la réussite "' ,
             tel: '+216 21 348 405',
-            email: 'mariem.aouadi.ma@gmail.com'},
+            email: 'mariem.aouadi.ma@gmail.com',
+            fb: 'https://www.facebook.com/mariem.aouadi' ,
+            lin: 'http://linkedin.com/in/mariem-aouadi-09a09814a' },
 
         {   name : 'Noura Bouthour' ,
             post : 'SG' ,
@@ -27,14 +31,18 @@ export class StructureComponent implements OnInit {
 quote: '"  Chaque bonne réalisation, grande ou petite, connait ses périodes de corvées et de triomphes;'
 + ' un début, un combat et une victoire."' ,
             tel: '+216 27 270 104',
-            email: 'nourabouthour@gmail.com' },
+            email: 'nourabouthour@gmail.com' ,
+            fb: 'https://www.facebook.com/profile.php?id=100001893592104' ,
+            lin: 'https://www.linkedin.com/in/noura-bouthour-759125183' },
 
         {   name : 'Marwen Rezgui' ,
             post : 'Trésorier' ,
             image: 'tresorier.JPG',
             quote: '" Je suis un guerrier et un insatisfait permanent, c\'est ça l\'apanage du champion. "' ,
             tel: '+216 25 601 877',
-            email: 'rezgui.marouen.12@gmail.com' },
+            email: 'rezgui.marouen.12@gmail.com' ,
+            fb: 'https://www.facebook.com/m.cl.r.ca' ,
+            lin: 'https://www.linkedin.com/in/marouen-rezgui-56851214b' },
 
 
 
@@ -44,14 +52,18 @@ quote: '"  Chaque bonne réalisation, grande ou petite, connait ses périodes de
             image: 'partenariat.JPG',
             quote: '" Tout est possible à qui rêve, ose, travaille et n\'abandonne jamais "' ,
             tel: '+216 53 530 405',
-            email: 'maya.bsalem@gmail.com' },
+            email: 'maya.bsalem@gmail.com' ,
+            fb: 'https://www.facebook.com/mayouta.bensalem' ,
+            lin: 'https://www.linkedin.com/in/maya-ben-salem-387876152' },
 
         {   name : 'Manel Ben Lakhdher' ,
             post : 'Responsable Logistique' ,
             image: 'logistique.JPG',
             quote: '" Quoi que tu rêves d’entreprendre, commence-le. L’audace a du génie, du pouvoir, de la magie. "' ,
             tel: '+216 56 302 634',
-            email: 'benlakhdar.manel.gm@gmail.com' },
+            email: 'benlakhdar.manel.gm@gmail.com' ,
+            fb: 'https://facebook.com/manel.benlakhdar.39?ref=bookmarks' ,
+            lin: 'https://www.linkedin.com/in/manel-ben-lakhdar-aa154a153' },
 
         /*{   name : 'Yasmin Mbarki' ,
             post : 'Responsable RH et Formations' ,
@@ -65,14 +77,18 @@ quote: '"  Chaque bonne réalisation, grande ou petite, connait ses périodes de
             image: 'communication.jpg',
             quote: '" Aller vers l’infini, ce n’est pas seulement avancer très loin, c’est avancer éternellement. "' ,
             tel: '+216 225 641 622',
-            email: 'firaseltaief1925@gmail.com'},
+            email: 'firaseltaief1925@gmail.com',
+            fb: 'https://www.facebook.com/eeltaief ' ,
+            lin: 'https://www.linkedin.com/in/firas-eltaief-448988147/' },
 
         {   name : 'Meriam Bettaieb' ,
             post : 'Responsable Projets et prospection' ,
             image: 'projet%20et%20prospection.JPG',
             quote: '" C\'est votre attitude, bien plus que votre aptitude, qui détermine votre altitude."' ,
             tel: '+216 50 660 123',
-            email: 'Meriam2.8bettaieb@gmail.com' },
+            email: 'Meriam2.8bettaieb@gmail.com' ,
+            fb: 'https://www.facebook.com/meriam.bettaieb ' ,
+            lin: 'https://www.linkedin.com/in/meriam-betta%C3%AFeb-035b08154/' },
     ];
   constructor() { }
 
